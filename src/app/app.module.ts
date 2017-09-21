2c98b24d6eacae6769601f5fbf0b91fd9d54c75d
Andrey Zagoruy <andreyzagoruy@Andreys-MacBook-Air.local>
2017-09-21 18:14:11 +0300

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FlowersListComponent } from './dashboard/flowers-list/flowers-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FlowerAddComponent } from './flower/flower-add/flower-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlowersListComponent,
+    NavbarComponent,
+    FlowerAddComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/flower/flower-add/flower-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'flower-add',
+  templateUrl: './flower-add.component.html',
+  styleUrls: ['./flower-add.component.scss']
+})
+export class FlowerAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
