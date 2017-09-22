f9c97b97ed55655b6c91742ed63b5a7b14fe5f58
Andrii Litvin <al@gogmat.com>
2017-09-22 18:01:58 +0300

    Fix

--- src/app/flower/flower-add/flower-add.component.ts
+++ src/app/flower/flower-add/flower-add.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'flower-add',
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlowerAddComponent implements OnInit {
 
+  @Input() flowerName = "";
+  @Input() oldWatering = "00";
+  @Input() nextWatering = "00";
+  @Input() today = new Date();
+  @Input() dayWatering = new Date();
+  @Input() lastWatering = new Date();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  flowers = [
+     {
+        flowerName : "cactus",
+        oldWatering : 22,
+        nextWatering : 23
+      },
+      {
+        flowerName : "violet",
+        oldWatering : 22,
+        nextWatering : 25
+      },
+      {
+        flowerName : "mandrake",
+        oldWatering : 22,
+        nextWatering : 24
+      }
+    ];
+
+  addFlower(today, dayWatering, lastWatering) {
+    this.flowers.push({
+      flowerName: this.flowerName,
+      oldWatering: lastWatering.setDate(today.getDate() -  Number(this.oldWatering)) ,
+      nextWatering: dayWatering.setDate(today.getDate()+ Number(this.nextWatering))
+    });
+    this.flowerName = '';
+    this.oldWatering = '00';
+    this.nextWatering = '00';
+  }
 }
